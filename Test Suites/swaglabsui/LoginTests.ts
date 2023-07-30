245d93d8effb110a723c3091bfabf7bccf0c94ce
Agent1997 <rommelmalked@gmail.com>
2023-07-30 00:35:41 -0400

    added tests

---
+++ Test Suites/swaglabsui/LoginTests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f4cac023-2ce0-4e9c-b7da-d4915bbb9af5</testSuiteGuid>
+   <testCaseLink>
+      <guid>d64d54e8-9643-4162-930e-cbb25f4db356</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/swaglabsui/login/SuccessfulLogin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
